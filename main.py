d6dea0367e084cb7986b5c49d859d3c05dcdfdf9
Violetta <salo46@yandex.ru>
2021-04-15 00:12:12 +0300

    почти доделанная первая кнопка

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import os
 from maps_api.geocoder import get_ll_span
 from weather import get_current_weather, get_forecast_weather
 
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 load_dotenv()
 keyboard1 = [['Пропустить']]
 keyboard2 = [['Показать на карте'], ['Найти ближайшую организацию'], ['Посчитать время на дорогу'], ['Погода'], ['Расписания'],
@@ -94,19 +105,21 @@ def static_photo(update, context):
             context.user_data['need_adresses'].append(text)
         else:
             context.user_data['need_adresses'] = [text]
-        update.message.reply_text(
-            'Выберите тип карты снимка:',
-            reply_markup=inline_maps
-        )
-        return GET_PHOTO
+    update.message.reply_text(
+        'Выберите тип карты снимка:',
+        parse_mode='markdown',
+        reply_markup=inline_maps
+    )
+    return GET_PHOTO_HANDLER
 
 
-def get_photo(update, context):
+def get_photo_handler(update, context):
     query = update.callback_query
-    print(query)
+    print(query.data)
+    print('here1')
     context.user_data['need_maptype'] = query.data
     ll, spn = get_ll_span(context.user_data['need_adresses'][-1])
-    static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l={context.user_data['need_maptype']}"
+    static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map"
     context.bot.send_photo(
         update.message.chat_id,
         static_api_request,
@@ -119,7 +132,7 @@ def need_adress(update, context):
     return STATIC_PHOTO
 
 
-def choosing_map_type(bot, update, user_data):
+def choose_map_type(bot, update, user_data):
     query = update.callback_query
     bot.edit_message_text(chat_id=query.message.chat_id, message_id=query.message.message_id,
                           text="[​​​​​​​​​​​]({}){}".format(get_static_map(user_data, query.data),
@@ -163,7 +176,7 @@ def main():
 
 
 (
-    ENTER_NAME, ENTER_LOCATION, MAIN_MENU, STATIC_PHOTO, NEED_ADRESS, GET_PHOTO
+    ENTER_NAME, ENTER_LOCATION, MAIN_MENU, STATIC_PHOTO, NEED_ADRESS, GET_PHOTO_HANDLER
 ) = range(6)
 
 
@@ -175,8 +188,9 @@ conversation_handler = ConversationHandler(
         MAIN_MENU: [MessageHandler(Filters.text, main_menu, pass_user_data=True)],
         ENTER_LOCATION: [MessageHandler(Filters.text, enter_location, pass_user_data=True)],
         STATIC_PHOTO: [MessageHandler(Filters.text, static_photo, pass_user_data=True)],
-        NEED_ADRESS: [MessageHandler(Filters.text, need_adress, pass_user_data=True)],
-        GET_PHOTO: [MessageHandler(Filters.text, get_photo, pass_user_data=True)]
+        GET_PHOTO_HANDLER: [MessageHandler(Filters.text, get_photo_handler, pass_user_data=True),
+            CallbackQueryHandler(choose_map_type, pass_user_data=True)],
+        NEED_ADRESS: [MessageHandler(Filters.text, need_adress, pass_user_data=True)]
     },
 
     fallbacks=[CommandHandler('stop', stop)]
